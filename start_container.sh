b50708d2ccf6ada3a0f63170d73e89c9af542c46
root <root@tyrion>
2016-01-24 13:37:43 -0500

    switched to https and port 30303

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-docker run -d -h grafana
+docker run -d -h grafana \
   -p 127.0.0.1:3030:3030 \
   -v /var/grafana/data:/var/lib/grafana \
   -v /var/grafana/logs:/var/log/grafana \
-  -e "GF_SERVER_ROOT_URL=http://grafana.r00t.ca"  \
+  -e "GF_SERVER_ROOT_URL=https://grafana.r00t.ca"  \
+  -e "GF_SERVER_HTTP_PORT=3030" \
   grafana
